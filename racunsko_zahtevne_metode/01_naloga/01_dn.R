dcc54caee003a947b74806efe4294a4d14555945
nezakrzan <nezza.krzzan@gmail.com>
2024-10-26 13:33:15 +0200

    popravek

--- racunsko_zahtevne_metode/01_naloga/01_dn.R
+++ racunsko_zahtevne_metode/01_naloga/01_dn.R
@@ -3,6 +3,8 @@ set.seed(2024)
 library(tidyr)
 library(ADGofTest)
 library(ggplot2)
+library(car)
+
 
 st_ponovitev = 5000
 velikost = c(15, 40, 60, 100, 200, 1000)
@@ -234,5 +236,175 @@ ggplot(valAnal_P, aes(y=power, col=method, group=method, x=n))+
   labs(title = " ",
        color = " ") + 
   theme_minimal() 
+
+######## Razlika med klasičnim ANOVA testom in Welchovim ANOVA testom ########
+############################### p - vrednost ##################################
+# var = (1,1,1)
+velikosti = c("100, 200, 1000", "20, 25, 30", "500, 500, 500")
+settings = expand.grid(i=1:st_ponovitev, n = velikosti)
+
+if(file.exists("p_vrednost_primerjava.RDS")){
+  res<-readRDS("p_vrednost_primerjava.RDS")
+} else {
+  res<-cbind(settings, pANOVA=NA, pANOVA_2=NA, pMETHOD3=NA)
+  for(row in 1:nrow(settings)){
+    # dolocitev velikosti skupin in varianc
+    velikost = as.numeric(strsplit(as.character(settings$n[row]),",")[[1]])
+    n1 = velikost[1]
+    n2 = velikost[2]
+    n3 = velikost[3]
+    sd1 = sqrt(1)
+    sd2 = sqrt(1)
+    sd3 = sqrt(1)
+    
+    # generiranje skupin
+    x<-c(rnorm(n1, 0, sd1),
+         rnorm(n2, 0, sd2),
+         rnorm(n3, 0, sd3))
+    gr<-factor(rep(1:3, times=c(n1,n2,n3)))
+    
+    # classic ANOVA
+    res$pANOVA[row] = summary(aov(x~gr))[[1]][["Pr(>F)"]][1]
+    
+    # Welch's ANOVA
+    res$pANOVA_2[row] = oneway.test(x~gr, var.equal = FALSE)$p.value
+    
+    # Levenejev test
+    p_test_enak_V = leveneTest(x~gr)$`Pr(>F)`[1]
+    if(p_test_enak_V > 0.05){
+      res$pMETHOD3[row] = summary(aov(x~gr))[[1]][["Pr(>F)"]][1]
+    } else {
+      res$pMETHOD3[row] = oneway.test(x~gr, var.equal = FALSE)$p.value
+    }
+  }
+  # shranjevanje
+  saveRDS(object = res, file="p_vrednost_primerjava.RDS")
+}
+
+# risanje p-vrednost: Prikaz p-vrednosti Anderson-Darling testa.
+resLong = pivot_longer(res, cols=matches("^p[AM]"), values_to = "pVal",
+                       names_to = "method", names_prefix = "p")
+resLong$method[resLong$method == "ANOVA"] = "classic ANOVA"
+resLong$method[resLong$method == "ANOVA_2"] = "Welch's ANOVA"
+resLong$method[resLong$method == "METHOD3"] = "Levene's test"
+
+levels(resLong$n) = c("n = (100, 200, 1000)", "n = (20, 25, 30)", "n = (500, 500, 500)")
+
+# testirava za enakomerno porazdelitev z Anderson-Darling testom
+valAnal = aggregate(pVal~ n + method , data = resLong,
+                    function(x)ad.test(x, distr.fun = punif)$p.value)
+valAnal$method[valAnal$method == "ANOVA"] = "classic ANOVA"
+valAnal$method[valAnal$method == "ANOVA_2"] = "Welch's ANOVA"
+valAnal$method[valAnal$method == "METHOD3"] = "Levene's test"
+
+# graf
+valAnal$n<-as.factor(valAnal$n)
+ggplot(valAnal, aes(y=pVal, col=method, group=method, x=n))+
+  geom_point()+geom_line()+
+  geom_hline(yintercept = 0.05)+
+  #facet_wrap(vars(n))+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  ylab("p-vrednost") + 
+  #xlab("velikost skupin(n)") + 
+  labs(title = " ",
+       color = " ") + 
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+############################### velikost testa ##################################
+valAnal = aggregate(pVal~ n + method , data = resLong,
+                    function(x)mean(x<0.05))
+valAnal$method[valAnal$method == "ANOVA"] = "classic ANOVA"
+valAnal$method[valAnal$method == "ANOVA_2"] = "Welch's ANOVA"
+valAnal$method[valAnal$method == "METHOD3"] = "Levene's test"
+
+levels(resLong$n) = c("n = (100, 200, 1000)", "n = (20, 25, 30)", "n = (500, 500, 500)")
+
+valAnal$n<-as.factor(valAnal$n)
+ggplot(valAnal, aes(y=pVal, col=method, group=method, x=n))+
+  geom_point()+geom_line()+#geom_smooth(se=F)+
+  geom_hline(yintercept = 0.05)+
+  #facet_wrap(vars(var)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  ylab("delez zavrnitve") +
+  xlab("velikost skupin(n)") + 
+  labs(title = " ",
+       color = " ") + 
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+############################### moc testa ##################################
+### enake variance
+povp = c("0,0.5,0", "0,-0.5,0.5")
+settings = expand.grid(i=1:st_ponovitev, n = velikosti, povp = povp)
+
+if(file.exists("moc_enake_primerjava.RDS")){
+  res_p<-readRDS("moc_enake_primerjava.RDS")
+} else {
+  res_p<-cbind(settings, pANOVA=NA, pANOVA_2=NA, pMETHOD3=NA)
+  for(row in 1:nrow(settings)){
+    # dolocitev velikost skupin, variance, povprecja
+    velikost = as.numeric(strsplit(as.character(settings$n[row]),",")[[1]])
+    n1 = velikost[1]
+    n2 = velikost[2]
+    n3 = velikost[3]
+    sd1 = sqrt(1)
+    sd2 = sqrt(1)
+    sd3 = sqrt(1)
+    povp_skupaj = as.numeric(strsplit(as.character(settings$povp[row]),",")[[1]])
+    povp1 = povp_skupaj[1]
+    povp2 = povp_skupaj[2]
+    povp3 = povp_skupaj[3]
+    
+    # generiranje vzorca
+    x<-c(rnorm(n1, povp1, sd1),
+         rnorm(n2, povp2, sd2),
+         rnorm(n3, povp3, sd3))
+    gr<-factor(rep(1:3, times=c(n1,n2,n3)))
+    
+    # classic ANOVA
+    res_p$pANOVA[row] = summary(aov(x~gr))[[1]][["Pr(>F)"]][1]
+    # Welch's ANOVA
+    res_p$pANOVA_2[row] = oneway.test(x~gr, var.equal = FALSE)$p.value
+    # Levene test
+    p_test_enak_V = leveneTest(x~gr)$`Pr(>F)`[1]
+    if(p_test_enak_V > 0.05){
+      res_p$pMETHOD3[row] = summary(aov(x~gr))[[1]][["Pr(>F)"]][1]
+    } else {
+      res_p$pMETHOD3[row] = oneway.test(x~gr, var.equal = FALSE)$p.value
+    }
+  }
+  # shranjevanje
+  saveRDS(object = res_p, file="moc_enake_primerjava.RDS")
+}
+
+# risanje moci enake var: Prikaz moči testa v primeru enakih varianc.
+resLong_P = pivot_longer(res_p, cols=matches("^p[AM]"), values_to = "power",
+                         names_to = "method", names_prefix = "p")
+levels(resLong_P$povp) = c("povprecja = (0,0.5,0)", "povprecja = (0,-0.5,0.5)")
+levels(resLong$n) = c("n = (100, 200, 1000)", "n = (20, 25, 30)", "n = (500, 500, 500)")
+
+valAnal_P = aggregate(power ~ n + method + povp, data = resLong_P,
+                      function(x)mean(x<0.05))
+valAnal_P$method[valAnal_P$method == "ANOVA"] = "classic ANOVA"
+valAnal_P$method[valAnal_P$method == "ANOVA_2"] = "Welch's ANOVA"
+valAnal_P$method[valAnal_P$method == "METHOD3"] = "Levene's test"
+
+# graf
+valAnal_P$n<-as.factor(valAnal_P$n)
+ggplot(valAnal_P, aes(y=power, col=method, group=method, x=n))+
+  geom_point()+geom_line()+
+  geom_hline(yintercept = 0.05)+
+  facet_wrap(vars(povp))+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  ylab("moc") +
+  xlab("velikost skupin(n)") + 
+  labs(title = " ",
+       color = " ") + 
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+
+
 
 
