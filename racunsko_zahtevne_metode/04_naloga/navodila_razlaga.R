0370319c4eb00a32da30d045bc676a51837354ef
nezakrzan <nezza.krzzan@gmail.com>
2024-11-29 10:18:20 +0100

    navodila

---
+++ racunsko_zahtevne_metode/04_naloga/navodila_razlaga.R
@@ -0,0 +1,8 @@
+## D.N. razmisli kaj je tvoja populacija, če imaš izbrane podatke....
+# - mora imati dovolj veliko vrednost missing values, ne smeš jih dodati!
+# - kaj je vzorec missing values, zato jih ne smes sam dodati...
+# - incom, gross pay je kao perfect data, ali podatki izven evrope, kaggle tudi
+# - uporabi free methods(uporabi tisto ki je najbolj primerna za problem ali vsaj eno med temi ki so najbolj primerne) - to bo odločalo za točke
+# - uporabi eno tisto ki so markirane, tudi če ni primerna, ampak jo morač(to lahko pod tretjo točko kjer je free)
+# - ampak verjetno bo ena izmed teh označenih najbolj primerna
+# - 
