b303276f3165b6d9c69f780882e9d65547c55947
nezakrzan <nezza.krzzan@gmail.com>
2024-11-29 11:46:58 +0100

    4. naloga

---
+++ racunsko_zahtevne_metode/04_naloga/04dn_Rupnik_Krzan.R
@@ -0,0 +1,23 @@
+# neki predlogov za podatke
+
+# 1. opacija: teh je pomojem precej premalo...
+library(mice)
+data = nhanes 
+summary(data)
+nrow(data)
+missing_data = (colSums(is.na(data)) / nrow(data)) * 100 # koliko % podatkov manka po stolpcih
+missing_data
+
+# 2. opcija: Pima Indians Diabetes Database - ta se mi zdi kr okej...
+data = read.csv("pima_indians_diabetes_dataset.csv", header = TRUE, sep = ",", na.strings = c(""))
+summary(data)
+nrow(data)
+missing_data = (colSums(is.na(data)) / nrow(data)) * 100 # koliko % podatkov manka po stolpcih
+missing_data
+# https://github.com/ashishpatel26/Pima-Indians-Diabetes-Dataset-Missing-Value-Imputation/blob/master/Readme.md
+# https://www.kaggle.com/code/faelk8/diabetes-eda-missing-values
+# https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database
+
+
+
+
